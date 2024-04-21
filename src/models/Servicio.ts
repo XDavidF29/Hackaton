f1508f48c3e1d48dd7782df20bb18406f587d175
FTTE27 <vasquezv.juanandres@gmail.com>
2024-04-21 17:09:37 -0500

    prueba

--- src/models/Servicio.ts
+++ src/models/Servicio.ts
@@ -1,4 +1,6 @@
 import { Proveedor } from "./Proveedor";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 export interface Servicio {
     id: number;
@@ -7,10 +9,20 @@ export interface Servicio {
     //imagenes: string[];
     precio: number;
     region: string;
-    tipo: string;
+    tipo: string; //plan o producto
     disponibilidad: boolean;
     calificacion: number;
     comentarios: string[];
     proveedor: Proveedor;
+  }
+  @Injectable({ providedIn: 'root' })
+  export class ServiciosService {
+    apiUrl = 'http://localhost:3000/servicios'; // Reemplaza con la URL de tu backend
+  
+    constructor(private http: HttpClient) {}
+  
+    getServicios() {
+      return this.http.get<Servicio[]>(this.apiUrl);
+    }
   }
   
