533b80afd3c5d9acd3df1b48e291099dbfda75e9
FTTE27 <vasquezv.juanandres@gmail.com>
2024-04-22 19:16:43 -0500

    componentes no reconoce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+
 import { AppComponent } from "./app.component";
+import { UserComponent } from "./user/user.component";
+import { ServicioProductoComponent } from "./ServicioProducto/ServicioProducto.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent,
+    UserComponent,
+    ServicioProductoComponent
+    
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserModule,
+  ]
 })
 export class AppModule { }
